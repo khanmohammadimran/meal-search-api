9d2919c322ed35651fac8fcbf9af31a8b3b3d815
khanmohammadimran <khanmohammadimran.i31@gmail.com>
2022-02-28 05:25:22 +0600

    error handling update

--- js/app.js
+++ js/app.js
@@ -1,31 +1,45 @@
+document.getElementById('error-message').style.display = 'none';
 const searchFood = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     // Clear Data 
     searchField.value = '';
+    document.getElementById('error-message').style.display = 'none';
+    if (searchText == '') {
+        alert('Please write a food name')
+        return;
+    }
 
     // Load Data 
-    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displaySearchResult(data.meals));
+    else {
+        const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => displaySearchResult(data.meals))
+            .catch(error => displayError(error));
+    }
 }
-
+const displayError = error => {
+    document.getElementById('error-message').style.display = 'block';
+}
 const displaySearchResult = meals => {
     const searchResult = document.getElementById('search-result');
     searchResult.textContent = '';
+    if (meals.length == 0) {
+
+    }
     meals.forEach(meal => {
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
-            <div onclick="loadMealDetail(${meal.idMeal})" class="card h-100 container">
-                <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">${meal.strMeal}</h5>
-                    <p class="card-text">${meal.strInstructions.slice(0, 200)}</p>
-                </div>
-            </div>
-        `;
+                    <div onclick="loadMealDetail(${meal.idMeal})" class="card h-100 container">
+                        <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">${meal.strMeal}</h5>
+                            <p class="card-text">${meal.strInstructions.slice(0, 200)}</p>
+                        </div>
+                    </div>
+                `;
         searchResult.appendChild(div)
     })
 }
